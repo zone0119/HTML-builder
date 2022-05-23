d0dce3c495e305d2a45f24209cbcfb1cdaf0e07d
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-05-24 02:39:33 +0600

    init: add async copy files and delete files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+const getDirName = __dirname;
+const fromFolderName = 'files';
+const toFolderName = 'files-copy';
+const newFolder = path.join(getDirName, toFolderName);
+const copyFromFolder = path.join(getDirName, fromFolderName);
 
+
+
+
+async function recursionDelFiles(folder) {
+
+    fs.readdir(folder, {withFileTypes: true}, (err, filesDel) => {
+        if(err)    {
+            throw err;
+        }
+        
+        filesDel.map(file => {      
+
+            const newCopiedFileDir = path.join(folder, file.name);
+            
+
+            if(file.isFile()) {                                
+
+                fs.promises.unlink(newCopiedFileDir);
+            }
+            else
+            {
+                if(file.isDirectory){
+
+                    recursionDelFiles(newCopiedFileDir);
+                    
+                }
+
+                
+            }
+
+
+        });
+
+
+
+    });
+    
+}
+
+
+async function init()
+{
+
+
+
+    fs.access(newFolder, (err) => {
+
+        if(err)
+        {
+            fs.promises.mkdir(newFolder, { recursive: true });
+        }
+        else
+        {
+            recursionDelFiles(newFolder);  
+        }
+        
+    });
+    
+    
+    
+    await fs.promises.mkdir(newFolder, { recursive: true });
+    
+
+    await copyFiles();
+
+
+}
+
+
+
+async function copyFiles() {
+    fs.readdir(copyFromFolder, {withFileTypes: true}, (err, files) => {
+        if(err)    {
+            throw err;
+        }
+    
+        files.map(file => {        
+            if(file.isFile()) {
+                const newFile = path.join(getDirName, fromFolderName, file.name);
+                const newCopiedFileDir = path.join(getDirName, toFolderName, file.name);
+    
+                fs.promises.copyFile(newFile, newCopiedFileDir);
+    
+                
+            }
+        });
+    
+    
+    });
+    
+}
+
+
+
+
+
+
+
+init();
