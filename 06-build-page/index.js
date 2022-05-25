9f213b9020bbc935240b37122fb8cfd7b5c5b6cf
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-05-25 11:14:36 +0600

    feat: add recursionCopyDirs

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -11,33 +11,132 @@ const getSrcStylesFolder = path.join(getDirName, 'styles');
 const encode = { encoding: 'UTF-8' };
 const writeStream = fs.createWriteStream(mergeStylesFolder, encode);
 
-const assetsFolder = path.join(getDirName, 'assets');
-const newAssetsFolder = path.join(getDirName, 'project-dist', 'assets');
+const projectDistAssetsFolder = path.join(getDirName, 'project-dist', 'assets');
+const assetsFolder = path.join(getDirName,  'assets');
+
+const TplFile = path.join(getDirName, 'template.html');
+
+const indexHTMLFile = path.join(getDirName, 'project-dist', 'index.html');
+
+
+
+
+let text = '';
+
+const componentFolder = path.join(getDirName, 'components');
+let arrCollectTPL= [];
+let objCollectTPL= {
+
+};
 
 async function init()
 {
 
     await fs.promises.mkdir(newFolder, { recursive: true });    
+    await fs.promises.mkdir(projectDistAssetsFolder, { recursive: true });    
 
-   // await fs.rm(newFolder, {recursive: true, force: true});
-
-    
     await recursionCopyDirs(assetsFolder);
     
-
-
     await copyStyles(getSrcStylesFolder);
 
     await recursionCopyFiles(assetsFolder);
 
-    console.log('Прошу данное задание проверить через день.');
+   getText();
+   await createObjComponent(componentFolder);
+  
+    
+
 
 }
 
 
+function getText(){
+
+    
+    const readStream = fs.createReadStream(TplFile, 'utf8');
+
+        readStream.on('data', (chunkwStream) => {                    
+                              
+                         text += chunkwStream;
+                                    
+        });
+            
+
+
+}
+
+async function templateFindReplace(objCollectTPL, file) {
+    
+
+
+                    const stopWord = '{{'  + file.name.substring(0, (file.name.length) - 5) + '}}';
+                                       
+                    let reg = new RegExp(stopWord, "g");
+
+                  
+
+                    text = text.replace(reg, objCollectTPL[stopWord]);                                  
+
+                    let writerStreamToIndexHtml = fs.createWriteStream(indexHTMLFile);    
+                    writerStreamToIndexHtml.write(text);
+
+
+        
+}
+
+ function createObjComponent() {
+
+    
+    fs.readdir(componentFolder, {withFileTypes: true}, (err, files) => {
+        if(err)    {
+            throw err;
+        }
+
+                files.map(file => {        
+                            if(file.isFile()) {                                                
+                                const newFile = path.join(getDirName, 'components', file.name);
+                                const getTPLStream = fs.createReadStream(newFile, 'utf8');
+                        
+                                const tpll =  fs.promises.readFile(newFile).then(res => { 
+                                    const stopWord = '{{'  + file.name.substring(0, (file.name.length) - 5) + '}}';
+                                     saveToObj(res, stopWord);
+
+
+                                    
+                                    templateFindReplace(objCollectTPL, file);
+
+
+                                });                                                                    
+                                
+                            }
+                    
+                    
+                                           
+                });
+
+                
+            
+        
+    });
+
+   
+}
+
+ function saveToObj(res, name){
+
+    let k =   name;
+     objCollectTPL[k] =     res.toString();
+     
+
+     arrCollectTPL.push(objCollectTPL);
+
+    
+}
+
+
+
 async function copyStyles(folder) {    
     
-console.log(folder);
 
     fs.readdir(folder, {withFileTypes: true}, (err, files) => {
         if(err)    {
